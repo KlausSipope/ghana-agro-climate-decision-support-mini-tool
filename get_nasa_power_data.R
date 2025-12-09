aa0af4d9197ff3a8366fb3dbde45fa0e2e756d9c
Klaus Sipope <sipopeklaus@gmail.com>
2025-12-09 17:53:32 +0000

    Add reusable function to fetch NASA POWER climate data by district name

---
+++ get_nasa_power_data.R
@@ -0,0 +1,23 @@
+library(sf)
+library(dplyr)
+library(nasapower)
+
+# 
+districts <- st_read("ghana-shapefile/gadm41_GHA_2.shp")
+
+# Get parameters from API call on NASAPOWER
+get_parameters<- function(district_name) {
+  coordinates <- districts %>% 
+    filter(NAME_2 == district_name) %>% 
+    st_centroid() %>%
+    st_coordinates()
+  
+  output <- get_power(
+    community = "AG",
+    lonlat = coordinates,
+    pars = c("T2M_MIN", "T2M_MAX", "PRECTOTCORR"),
+    dates = c("2015-01-01", "2024-12-31"),
+    temporal_api = "daily"
+  )
+}
+
